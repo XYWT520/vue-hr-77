db8494303d4107f8621678fd89db2e180bdfb2ba
ZYC <2095921712@qq.com>
2022-06-09 10:47:34 +0800

    feat:权限管理已完成

--- src/views/permissions/permissions.vue
+++ src/views/permissions/permissions.vue
@@ -4,7 +4,7 @@
       <!-- 表格 -->
       <el-card>
         <div style="text-align: right; margin-bottom: 20px">
-          <el-button type="primary" size="small" @click="hAdd">添加权限</el-button>
+          <el-button type="primary" size="small" @click="hAdd(1, '0')">添加权限</el-button>
         </div>
         <el-table border :data="permissionsList" row-key="id">
           <el-table-column label="名称" prop="name" />
@@ -12,15 +12,16 @@
           <el-table-column label="描述" prop="code" />
           <el-table-column label="操作">
             <template v-slot="{row}">
-              <el-button v-if="row.type === 1" type="text">添加</el-button>
-              <el-button type="text">编辑</el-button>
-              <el-button type="text">删除</el-button>
+              <el-button v-if="row.type === 1" type="text" @click="hAdd(2,row.id)">添加</el-button>
+              <el-button type="text" @click="hEdit(row.id)">编辑</el-button>
+              <el-button type="text" @click="hDel(row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
       </el-card>
     </div>
-    <el-dialog :visible.sync="showDialog" title="弹层标题">
+
+    <el-dialog :visible.sync="showDialog" title="弹层标题" @close="close">
       <!-- 表单内容 -->
       <el-form ref="form" :model="formData" label-width="100px" :rules="rules">
         <el-form-item label="权限名称" prop="name">
@@ -54,13 +55,14 @@
 </template>
 
 <script>
-import { getPermissionList } from '@/api/permissions'
+import { getPermissionList, addPermission, delPermission, getPermissionDetail, updatePermission } from '@/api/permissions'
 import { tranListToTreeData } from '@/utils'
 export default {
   data() {
     return {
       permissionsList: [],
       showDialog: false,
+      isEdit: false,
       formData: {
         name: '', // 名称
         code: '', // 权限标识
@@ -94,13 +96,87 @@ export default {
       this.permissionsList = tranListToTreeData(res.data)
     },
 
-    hAdd() {
+    // 两个添加按钮
+    hAdd(type, pid) {
       this.showDialog = true
+      this.formData.type = type
+      this.formData.pid = pid
+      this.isEdit = false
     },
+
     // 添加权限 兜底校验
     async hSubmit() {
       const valid = await this.$refs.form.validate().catch(e => e)
       if (!valid) return
+
+      this.isEdit ? this.doEdit() : this.doAdd()
+    },
+
+    // 添加
+    async doAdd() {
+      try {
+        // 发请求
+        const res = await addPermission(this.formData)
+        // 提醒用户
+        this.$message.success(res.message)
+        // 关闭 dialog
+        this.showDialog = false
+        // 重现获取页面
+        this.loadgetPermissionList()
+      } catch (e) {
+        this.$message.error(e.message)
+      }
+    },
+
+    // 编辑
+    async doEdit() {
+      try {
+        // 发请求
+        const res = await updatePermission(this.formData)
+        // 提醒用户
+        this.$message.success(res.message)
+        // 关闭 dialog
+        this.showDialog = false
+        // 重现获取页面
+        this.loadgetPermissionList()
+      } catch (e) {
+        this.$message.error(e.message)
+      }
+    },
+
+    // 编辑按钮
+    async hEdit(id) {
+      this.isEdit = true
+      const res = await getPermissionDetail(id)
+      // console.log(res)
+      this.formData = res.data
+      this.showDialog = true
+    },
+
+    // 删除按钮
+    async hDel(id) {
+      const reluts = await this.$confirm('确定删除?', '提示', { type: 'warning' }).catch(e => e)
+      if (reluts !== 'confirm') return
+      try {
+        const res = await delPermission(id)
+        this.$message.success(res.message)
+        this.loadgetPermissionList()
+      } catch (e) {
+        this.$message.error(e.message)
+      }
+    },
+
+    // 清空表单
+    close() {
+      this.formData = {
+        name: '', // 名称
+        code: '', // 权限标识
+        description: '', // 描述
+        enVisible: '0', // 开启
+        pid: '', // 添加到哪个节点下
+        type: '' // 类型
+      }
+      this.$refs.form.clearValidate()
     }
   }
 }
